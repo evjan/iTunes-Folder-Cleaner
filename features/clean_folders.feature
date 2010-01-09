e41a80e03478f624863d1520bd7d56b67c35963f
Peter Hultgren <peter.evjan@gmail.com>
2010-01-09 17:13:00 +0100

    keeps a folder if it contains an mp3 file directly beneath it

--- features/clean_folders.feature
+++ features/clean_folders.feature
@@ -3,7 +3,12 @@ Feature: Clean Folders
   When I have a lot of empty music folders that iTunes couldn't clean up for me
   Then those folders and their contents should be deleted
 
-  Scenario: Remove a folder with no mp3's in it
+  Scenario: Remove an empty folder
     Given an empty folder
     When I clean it
     Then it should be removed
+
+  Scenario: Keep a folder with an audio file in it
+    Given a folder with an mp3 file in it
+    When I clean it
+    Then it should still be there
--- features/step_definitions/folder_cleaner_steps.rb
+++ features/step_definitions/folder_cleaner_steps.rb
@@ -3,9 +3,13 @@ require 'folder_cleaner'
 require 'dir_handler'
 
 Given /^an empty folder$/ do
-  @path = File.join(File.expand_path(File.dirname(__FILE__)),  "testfolder")
-  Dir.delete(@path) if File.directory? @path
-  Dir.mkdir(@path)
+  @path = create_folder("testfolder")
+  @dir_handler = DirHandler.new
+end
+
+Given /^a folder with an mp3 file in it$/ do
+  @path = create_folder("testfolder")
+  FileUtils.touch(File.join(@path, "test.mp3"))
   @dir_handler = DirHandler.new
 end
 
@@ -15,4 +19,15 @@ end
 
 Then /^it should be removed$/ do
   (File.directory? @path).should == false
+end
+
+Then /^it should still be there$/ do
+  (File.directory? @path).should == true
+end
+
+def create_folder(folder_name)
+  @path = File.join(File.expand_path(File.dirname(__FILE__)),  folder_name)
+  FileUtils.remove_dir(@path) if File.directory? @path
+  FileUtils.mkdir(@path)
+  @path
 end
